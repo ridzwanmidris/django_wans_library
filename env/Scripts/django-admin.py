f1e18a7e33b205c98cf040eca905dcbb567aab33
winzardv11 <ridzwan.midris@gmail.com>
2019-05-08 23:44:22 +0800

    add ignore

---
+++ env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\user\OneDrive\Personal\Coding\Python Projects\winzardv11 git hub repos\django_wans_library\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
