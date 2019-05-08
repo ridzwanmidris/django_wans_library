cd49ea3354eff30bc5cc5b29513d96a58a8c716f
winzardv11 <ridzwan.midris@gmail.com>
2019-05-08 23:08:37 +0800

    added import function at admin

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,6 @@
+from import_export.admin import ImportExportModelAdmin
+from import_export import resources, fields
+from import_export.widgets import ForeignKeyWidget
 from django.contrib import admin
 
 # Register your models here.
@@ -6,13 +9,19 @@ from catalog.models import Author, Genre, Book, BookInstance
 
 # admin.site.register(Book)
 # admin.site.register(Author)
-admin.site.register(Genre)
+# admin.site.register(Genre)
 # admin.site.register(BookInstance)
 
 # Define the admin class
 
 
-class AuthorAdmin(admin.ModelAdmin):
+class AuthorResource(resources.ModelResource):
+    class Meta:
+        model = Author
+
+
+class AuthorAdmin(ImportExportModelAdmin):
+    resource_class = AuthorResource
     list_display = ('last_name', 'first_name',
                     'date_of_birth', 'date_of_death')
     fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
@@ -41,7 +50,36 @@ class BooksInstanceInline(admin.TabularInline):
     model = BookInstance
 
 
-@admin.register(Book)
-class BookAdmin(admin.ModelAdmin):
+# @admin.register(Book)
+# class BookAdmin(admin.ModelAdmin):
+#    list_display = ('title', 'author', 'display_genre')
+#    inlines = [BooksInstanceInline]
+
+
+class BookResource(resources.ModelResource):
+
+    class Meta:
+        model = Book
+
+
+class BookAdmin(ImportExportModelAdmin):
+    resource_class = BookResource
     list_display = ('title', 'author', 'display_genre')
     inlines = [BooksInstanceInline]
+
+
+admin.site.register(Book, BookAdmin)
+
+
+class GenreResource(resources.ModelResource):
+
+    class Meta:
+        model = Genre
+
+
+class GenreAdmin(ImportExportModelAdmin):
+    resource_class = GenreResource
+    list_display = ('id', 'name')
+
+
+admin.site.register(Genre, GenreAdmin)
